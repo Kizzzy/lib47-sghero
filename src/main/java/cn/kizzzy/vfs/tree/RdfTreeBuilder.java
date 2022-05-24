e3d7c8317c7c19171566d71b3c23293ec662f58c
Kizzzy <xuhua0174@163.com>
2022-05-25 04:15:36 +0800

    [sghero] * update tree builder

--- src/main/java/cn/kizzzy/vfs/tree/RdfTreeBuilder.java
+++ src/main/java/cn/kizzzy/vfs/tree/RdfTreeBuilder.java
@@ -5,7 +5,7 @@ import cn.kizzzy.sghero.RdfFileItem;
 import cn.kizzzy.vfs.ITree;
 import cn.kizzzy.vfs.Separator;
 
-public class RdfTreeBuilder extends TreeBuilder {
+public class RdfTreeBuilder extends TreeBuilderAdapter<RdfFile, RdfFileItem> {
     
     private final RdfFile rdfFile;
     
@@ -20,31 +20,22 @@ public class RdfTreeBuilder extends TreeBuilder {
     
     @Override
     public ITree build() {
-        Root root = new Root(idGenerator.getId(), rdfFile.pkg);
-        
-        for (RdfFileItem file : rdfFile.headers) {
-            listImpl(root, root, file);
-        }
-        
-        return new Tree(root, separator);
-    }
-    
-    private void listImpl(Root root, Node parent, RdfFileItem file) {
-        String[] names = separator.split(file.path);
-        for (String name : names) {
-            Node child = parent.children.get(name);
-            if (child == null) {
-                if (name.contains(".")) {
-                    Leaf leaf = new Leaf(idGenerator.getId(), name, root.name, file.path, file);
-                    root.fileKvs.put(file.path, leaf);
-                    child = leaf;
-                } else {
-                    child = new Node(idGenerator.getId(), name);
-                }
-                root.folderKvs.put(child.id, child);
-                parent.children.put(name, child);
+        return buildImpl(rdfFile, new Helper<RdfFile, RdfFileItem>() {
+            
+            @Override
+            public String idxPath(RdfFile idxFile) {
+                return idxFile.pkg;
             }
-            parent = child;
-        }
+            
+            @Override
+            public Iterable<RdfFileItem> entries(RdfFile idxFile) {
+                return idxFile.headers;
+            }
+            
+            @Override
+            public String itemPath(RdfFileItem item) {
+                return item.path;
+            }
+        });
     }
 }
