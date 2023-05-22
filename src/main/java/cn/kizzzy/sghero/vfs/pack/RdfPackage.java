664829a61c537f4a826b53038853a5806bc867b7
Kizzzy <xuhua0174@163.com>
2023-05-22 22:19:12 +0800

    [sghero] * fix FileStreamGetterFactory

--- src/main/java/cn/kizzzy/sghero/vfs/pack/RdfPackage.java
+++ src/main/java/cn/kizzzy/sghero/vfs/pack/RdfPackage.java
@@ -9,7 +9,7 @@ import cn.kizzzy.vfs.stream.FileStreamGetterFactory;
 public class RdfPackage extends LeafPackage<RdfFile.Entry> {
     
     public RdfPackage(String root, ITree tree) {
-        this(tree, new FileStreamGetterFactory(root, tree));
+        this(tree, new FileStreamGetterFactory(root));
     }
     
     public RdfPackage(ITree tree, IStreamGetterFactory factory) {
